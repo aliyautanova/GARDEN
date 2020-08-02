767e60fc019b3a33b74b2eeaabfb5fa3233bd256
Aliya Utanova <alia.utanova@yandex.ru>
2020-08-02 16:56:15 +0300

    добавит попап с видео

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
   $('.slider').slick();
 });
+const popup = document.querySelector('.popup');
+const popupVideo = document.querySelector('.popup__container');
+const escapeButton = document.querySelector('.popup__escape');
+const openButton = document.querySelector('.about__button');
+
+//закрыть форму через esc
+function closeByEsc(evt) {
+  if (evt.key === 'Escape') {
+    document.querySelector('.popup_opened').classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeByEsc)
+  };
+};
+
+//открыть/закрыть
+function togglePopup(element) {
+  if (!element.classList.contains('popup_opened')) {
+    element.classList.add('popup_opened');
+    document.addEventListener('keydown', closeByEsc);
+  }
+  else {
+    element.classList.remove('popup_opened');
+  }
+}
+
+//закрыть форму через оверлэй
+document.addEventListener('click', function (evt) {
+  evt.target.classList.remove('popup_opened');
+  //остановим всплытие
+  evt.stopPropagation();
+});
+
+openButton.addEventListener('click', () => togglePopup(popup));
+escapeButton.addEventListener('click', () => togglePopup(popup));
